ab6cb68afb81d1d7abe8942aad503f9a764e5830
Anason Chima <anasonchima@Anasons-MacBook-Pro.local>
2021-02-23 11:55:19 +0800

    First commit.

---
+++ LearningBlogBackend/src/main/java/com/llb/fllbwebsite/FllbwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.llb.fllbwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FllbwebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FllbwebsiteApplication.class, args);
+    }
+
+}
